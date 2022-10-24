d95e0d7ea4ee924edb1f125af7687272aeb2263d
Matthew55 <78285385+matthew55@users.noreply.github.com>
2022-10-24 18:20:18 -0400

    Fix untracked files

--- config.h
+++ config.h
@@ -4,14 +4,12 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
-        "FontAwesome:pixelsize=10",
-        "Unifont:size=10"
+	"monospace:size=13",
+	"JoyPixels:pixelsize=13:antialias=true:autohint=true",
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
@@ -26,7 +24,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
